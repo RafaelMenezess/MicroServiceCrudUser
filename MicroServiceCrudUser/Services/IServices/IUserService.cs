9d36d6c33d4d0c928527536286b95f279eb9f569
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-15 20:59:28 -0300

    Adicionando IUserService

---
+++ MicroServiceCrudUser/Services/IServices/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using MicroServiceCrudUser.Models;
+
+namespace MicroServiceCrudUser.Services.IServices;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User> GetUserById(int id);
+    Task<User> CreateUser(User user);
+    Task<bool> UpdateUser(User user);
+    Task<bool> DeleteUser(int id);
+    Task<string> GenerateToken(User user);
+    Task<bool> ChangePassword(int userId, string newPassword);
+}
