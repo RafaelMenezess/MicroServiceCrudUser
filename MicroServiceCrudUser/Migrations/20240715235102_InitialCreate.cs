525a683f265f9471fb7a4a29ec9f8098608fbdaa
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-15 20:52:24 -0300

    Primeira migration

---
+++ MicroServiceCrudUser/Migrations/20240715235102_InitialCreate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MicroServiceCrudUser.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PasswordHash = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "PasswordHash", "Username" },
+                values: new object[,]
+                {
+                    { 1, "john.doe@example.com", "hash1", "johndoe" },
+                    { 2, "jane.smith@example.com", "hash2", "janesmith" },
+                    { 3, "michael.brown@example.com", "hash3", "michaelbrown" },
+                    { 4, "lisa.jones@example.com", "hash4", "lisajones" },
+                    { 5, "chris.miller@example.com", "hash5", "chrismiller" },
+                    { 6, "patricia.wilson@example.com", "hash6", "patriciawilson" },
+                    { 7, "robert.taylor@example.com", "hash7", "roberttaylor" },
+                    { 8, "linda.moore@example.com", "hash8", "lindamoore" },
+                    { 9, "william.thomas@example.com", "hash9", "williamthomas" },
+                    { 10, "barbara.clark@example.com", "hash10", "barbaraclark" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
--- MicroServiceCrudUser/Models/Context/MySqlContext.cs
+++ MicroServiceCrudUser/Models/Context/MySqlContext.cs
@@ -7,4 +7,81 @@ public class MySQLContext : DbContext
     public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
     public DbSet<User> Users { get; set; }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasData(
+            new User
+            {
+                Id = 1,
+                Username = "johndoe",
+                PasswordHash = "hash1",
+                Email = "john.doe@example.com"
+            },
+            new User
+            {
+                Id = 2,
+                Username = "janesmith",
+                PasswordHash = "hash2",
+                Email = "jane.smith@example.com"
+            },
+            new User
+            {
+                Id = 3,
+                Username = "michaelbrown",
+                PasswordHash = "hash3",
+                Email = "michael.brown@example.com"
+            },
+            new User
+            {
+                Id = 4,
+                Username = "lisajones",
+                PasswordHash = "hash4",
+                Email = "lisa.jones@example.com"
+            },
+            new User
+            {
+                Id = 5,
+                Username = "chrismiller",
+                PasswordHash = "hash5",
+                Email = "chris.miller@example.com"
+            },
+            new User
+            {
+                Id = 6,
+                Username = "patriciawilson",
+                PasswordHash = "hash6",
+                Email = "patricia.wilson@example.com"
+            },
+            new User
+            {
+                Id = 7,
+                Username = "roberttaylor",
+                PasswordHash = "hash7",
+                Email = "robert.taylor@example.com"
+            },
+            new User
+            {
+                Id = 8,
+                Username = "lindamoore",
+                PasswordHash = "hash8",
+                Email = "linda.moore@example.com"
+            },
+            new User
+            {
+                Id = 9,
+                Username = "williamthomas",
+                PasswordHash = "hash9",
+                Email = "william.thomas@example.com"
+            },
+            new User
+            {
+                Id = 10,
+                Username = "barbaraclark",
+                PasswordHash = "hash10",
+                Email = "barbara.clark@example.com"
+            }
+        );
+    }
 }
