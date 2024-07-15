90bc7cfd24c4bd234c45ea84c541832d474301e9
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-15 20:36:43 -0300

    Adicionando MySqlContext e editando o Progam

---
+++ MicroServiceCrudUser/Models/Context/MySqlContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MicroServiceCrudUser.Models.Context;
+
+public class MySQLContext : DbContext
+{
+    public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
