2be04957712f8cea084c6e26d966164b711b531c
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-16 11:36:45 -0300

    Alterando informações Swagger Program

--- MicroServiceCrudUser/Program.cs
+++ MicroServiceCrudUser/Program.cs
@@ -19,8 +19,6 @@ builder.Services.AddDbContext<MySQLContext>(options => options.
 
 builder.Services.AddScoped<IUserService, UserService>();
 
-builder.Services.AddControllers();
-
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -38,10 +36,21 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "User", Version = "v1" });
+    c.SwaggerDoc("v1", new OpenApiInfo 
+    {
+        Title = "Crud User",
+        Version = "v1",
+        Description = "A simple example ASP.NET Core Web API",
+        Contact = new OpenApiContact
+        {
+            Name = "Rafael Menezes",
+            Email = "rafa.rcm@hotmail.com",           
+        }
+    });
     c.EnableAnnotations();
 });
 
