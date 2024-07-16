501520d51b98558391dbde7100f9439d39c5004c
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-16 15:34:54 -0300

    Adicionando teste GetUsers

--- TestMicroServiceCrudUser/UserControllerTests.cs
+++ TestMicroServiceCrudUser/UserControllerTests.cs
@@ -1,6 +1,40 @@
-﻿namespace TestMicroServiceCrudUser
+﻿using MicroServiceCrudUser.Controllers;
+using MicroServiceCrudUser.Models;
+using MicroServiceCrudUser.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace TestMicroServiceCrudUser;
+
+public class UserControllerTests
 {
-    internal class UserControllerTests
+    private readonly UsersController _controller;
+    private readonly Mock<IUserService> _mockUserService;
+
+    public UserControllerTests()
     {
+        _mockUserService = new Mock<IUserService>();
+        _controller = new UsersController(_mockUserService.Object);
+    }
+
+    [Fact]
+    public async Task GetUsers()
+    {
+        // Arrange
+        var users = new List<User>
+            {
+                new User { Id = 1, Username = "user1" },
+                new User { Id = 2, Username = "user2" }
+            };
+        _mockUserService.Setup(service => service.GetAllUsers()).ReturnsAsync(users);
+
+        // Act
+        var result = await _controller.GetUsers();
+
+        // Assert
+        var actionResult = Assert.IsType<ActionResult<IEnumerable<User>>>(result);
+        var returnValue = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var returnUsers = Assert.IsType<List<User>>(returnValue.Value);
+        Assert.Equal(2, returnUsers.Count);
     }
 }
