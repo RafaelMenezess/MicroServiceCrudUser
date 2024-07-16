c7cde217c2a3ee7c63963c9aa1d648760e8959c2
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-15 21:01:31 -0300

    Criando a classe UserService

---
+++ MicroServiceCrudUser/Services/UserService.cs
@@ -0,0 +1,39 @@
+﻿using MicroServiceCrudUser.Models;
+using MicroServiceCrudUser.Services.IServices;
+
+namespace MicroServiceCrudUser.Services;
+
+public class UserService : IUserService
+{
+    public Task<IEnumerable<User>> GetAllUsers()
+    {
+        throw new NotImplementedException();
+    }
+    public Task<User> GetUserById(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<User> CreateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<bool> ChangePassword(int userId, string newPassword)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GenerateToken(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
