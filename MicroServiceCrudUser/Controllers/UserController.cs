68fc9019d3ba4c10cc51846d0c3f747e386e787d
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-15 21:12:41 -0300

    Criando UserController

---
+++ MicroServiceCrudUser/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using MicroServiceCrudUser.Models;
+using MicroServiceCrudUser.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroServiceCrudUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return Ok();
+        }
+    }
+
+}
--- MicroServiceCrudUser/Program.cs
+++ MicroServiceCrudUser/Program.cs
@@ -1,5 +1,8 @@
 using MicroServiceCrudUser.Models.Context;
+using MicroServiceCrudUser.Services;
+using MicroServiceCrudUser.Services.IServices;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +13,18 @@ builder.Services.AddDbContext<MySQLContext>(options => options.
                                    new MySqlServerVersion(
                                    new Version(8, 0, 2))));
 
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddControllers();
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "User", Version = "v1" });
+    c.EnableAnnotations();
+});
 
 var app = builder.Build();
 
@@ -25,5 +36,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
