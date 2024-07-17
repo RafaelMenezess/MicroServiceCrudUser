bc93ee047ebdd69621a3c85f8eda1d70ddef0aaa
Rafael Menezes <rafa.rcm@hotmail.com>
2024-07-16 21:18:26 -0300

    Adicionando cofig Serilog e OpenTelemetry

--- MicroServiceCrudUser/Program.cs
+++ MicroServiceCrudUser/Program.cs
@@ -5,10 +5,48 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
 using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Serilog
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("UserMicroservice"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddJaegerExporter(o =>
+            {
+                o.AgentHost = builder.Configuration["Jaeger:Host"];
+                o.AgentPort = int.Parse(builder.Configuration["Jaeger:Port"]);
+            });
+            //.AddConsoleExporter();
+    })
+    .WithMetrics(metricsProviderBuilder =>
+    {
+        metricsProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("UserMicroservice"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation();
+            //.AddConsoleExporter();
+    });
+
 
 var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
 
@@ -40,7 +78,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo 
+    c.SwaggerDoc("v1", new OpenApiInfo
     {
         Title = "Crud User",
         Version = "v1",
@@ -48,7 +86,7 @@ builder.Services.AddSwaggerGen(c =>
         Contact = new OpenApiContact
         {
             Name = "Rafael Menezes",
-            Email = "rafa.rcm@hotmail.com",           
+            Email = "rafa.rcm@hotmail.com",
         }
     });
     c.EnableAnnotations();
